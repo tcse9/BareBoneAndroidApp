ceade046f9e14bc96c86573cea734f6fffa71bb5
tcse9 <ovioviovi@gmail.com>
2019-08-06 02:10:58 +0600

    1. Changed AppInstanceIdService.java for new firebase implementation. 2. In AndroidManifest.xml removed previously added firebase related services and added new service.

--- app/src/main/java/com/barebone/app/firebase/AppInstanceIdService.java
+++ app/src/main/java/com/barebone/app/firebase/AppInstanceIdService.java
@@ -1,20 +1,49 @@
 package com.barebone.app.firebase;
 
+import android.content.Context;
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class AppInstanceIdService extends FirebaseInstanceIdService {
+public class AppInstanceIdService extends FirebaseMessagingService {
+
 
     @Override
-    public void onTokenRefresh() {
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.e("ID_TOKEN", "Refreshed token: " + refreshedToken);
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("newToken", s);
+        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fb", s).apply();
+    }
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.e("FIREBASE_MESSAGE", "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.e("FIREBASE_MESSAGE", "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+
+                //scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+
+                //handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.e("FIREBASE_MESSAGE", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    public static String getToken(Context context) {
+        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fb", "empty");
     }
 }
