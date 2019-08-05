eb44c56add2fee5e763462a4b5bd89cf69591a13
tcse9 <ovioviovi@gmail.com>
2019-08-06 02:23:17 +0600

    1. Changed firebase service class name to a meaningful name. 2. Removed unnecessary codes AndroidManifest.xml file.

--- app/src/main/java/com/barebone/app/firebase/AppFirebaseMessagingServiceService.java
+++ app/src/main/java/com/barebone/app/firebase/AppFirebaseMessagingServiceService.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class AppInstanceIdService extends FirebaseMessagingService {
+public class AppFirebaseMessagingServiceService extends FirebaseMessagingService {
 
 
     @Override
